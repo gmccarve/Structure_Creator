4414eff86638707ef552333fc81a916f2e36d11e
gmccarve <gmccarve@vols.utk.edu>
2020-05-15 14:28:16 -0400

    Fixed input from ligand file

--- Comp.py
+++ Comp.py
@@ -1,18 +1,13 @@
 import os
 import sys
+import itertools
 from LigMod import LIGMOD
+from CoreMod import COREMOD
+from FullMod import FULLMOD
+
 
 
 def COMP(ligand_dict, core_dict, mod_dict, param_dict):
-
-    def No_Inp(x):
-        while x == '':
-            x = str(input("\033[A > ")).lower()
-        return x
-
-    def clear():
-        os.system('clear')
-
 
     try:
         ligand_dict[0]
@@ -20,43 +15,45 @@ def COMP(ligand_dict, core_dict, mod_dict, param_dict):
         ligand_dict[0] = {}
 
     try:
-        mod_dict['Number of Substitutions']
-    except:
-        mod_dict['Number of Substitutions'] = []
-
-    try:
         mod_dict['Modifications']
-        if mod_dict['Number of Substitutions'] == []:
+        try:
+            mod_dict['Number of Substitutions']
+        except:
             mod_dict['Number of Substitutions'] = [1]
     except:
         mod_dict['Modifications'] = []
+        mod_dict['Number of Substitutions'] = []
 
-
-    num_atoms = 0
+    num_lig_atoms = 0
     for k, v in ligand_dict.items():
-        for kk, vv in v.items():
-            for kkk, vvv in vv.items():
-                if kkk == 'Non-Symmetric Hs' or kkk == 'Symmetric Hs':
-                    num_atoms += len(vvv)
+        for kk in v:
+            for kkk, vvv in kk.items():
+                for kkkk, vvvv in vvv.items():
+                    if kkkk == 'Non-Symmetric Hs':
+                        if vvvv != []:
+                            num_lig_atoms += len(vvvv)
+                    elif kkkk == 'Symmetric Hs':
+                        if vvvv != []:
+                            try:
+                                num_lig_atoms += len(list(itertools.chain.from_iterable(vvvv)))
+                            except:
+                                num_lig_atoms += len(vvvv)
 
+    num_core_atoms = 0
     for k, v in core_dict.items():
         for kk, vv in v.items():
             if kk == 'Non-Symmetric Hs' or kk == 'Symmetric Hs':
-                num_atoms += len(vv)
+                if vv != []:
+                    num_core_atoms += len(vv)
 
-    try:
-        FAIL = 0
-        mod_dict['Number of Substitutions']
+    num_atoms = num_core_atoms + num_lig_atoms
 
-        if len(mod_dict['Number of Substitutions']) > num_atoms:
-            print ("\n\n ERROR. Too many substitutions and not enough atoms to modify")
-            FAIL = 1
-
-    except:
-        FAIL = 0
-
-    if FAIL == 1:
-        sys.exit()
+    if len(mod_dict['Number of Substitutions']) > num_atoms:
+        print ("\n\n ERROR. Too many substitutions and not enough atoms to modify\
+                  \n Changing number of substitutions to match number of given\
+                  \n modifications")
+        mod_dict['Number of Substitutions'] = num_atoms
+    
 
 
     for core, core_mod in core_dict.items():
@@ -64,46 +61,30 @@ def COMP(ligand_dict, core_dict, mod_dict, param_dict):
         if not os.path.exists(core):
             os.mkdir(core)
 
-        for k, v in ligand_dict.items():
-
-            lig_count = 0
-            for kk, vv in v.items():
-                for kkk, vvv in vv.items():
-                    if kkk != 'Number of Ligands':
-                        lig_count += len(vvv)
         
-        core_count = 0
-        for k, v in core_mod.items():
-            core_count += len(v)
-
-        if lig_count == 0:
-            COREMOD(core, core_mod, ligand_dict, mod_dict)
-
-        elif core_count == 0:
-            LIGMOD(core, ligand_dict, mod_dict)
-
+        if num_lig_atoms == 0:
+            COREMOD(core, core_mod, ligand_dict, mod_dict, param_dict)
+        elif num_core_atoms == 0:
+            LIGMOD(core, ligand_dict, mod_dict, param_dict)
         else:
-            FULLMOD(core, core_mod, ligand_dict, mod_dict)
+            FULLMOD(core, core_mod, ligand_dict, mod_dict, param_dict)
 
-
-
-    sys.exit()
 
 
 if __name__ == '__main__':
 
 
-    ligand_dict = {0:
-                  {'acac': {'Number of Ligands': '1', 'Symmetric Hs': [7, 8], 'Non-Symmetric Hs': [14]}, 
-                   'no3': {'Number of Ligands': '2'}}}
-    
-    core_dict   = {'la': {'Symmetric Hs': [], 'Non-Symmetric Hs': []},
+    ligand_dict = {0: [{'acac': {'Ligand Frequency': '3', 'Symmetric Hs': [[1, 2, 3], [4, 5, 6]], 'Non-Symmetric Hs': [14]}}]}
+
+    core_dict   = {'la': {'Symmetric Hs': [1], 'Non-Symmetric Hs': []},
                    'gd': {'Symmetric Hs': [], 'Non-Symmetric Hs': []},
                    'lu': {'Symmetric Hs': [], 'Non-Symmetric Hs': []}}
 
 
     mod_dict    = {'Modifications': ['f', 'cl', 'br'], 'Number of Substitutions': [1]}
 
-    COMP(ligand_dict, core_dict, mod_dict)
+    param_dict  = {'geometry': 'oct'}
+
+    COMP(ligand_dict, core_dict, mod_dict, param_dict)
 
 
