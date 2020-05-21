28b9ca080f3e5e75d1a4c1c34ee72cb55a930a7e
gmccarve <gmccarve@vols.utk.edu>
2020-05-21 09:30:17 -0400

    Added correction functions

--- CoreMod.py
+++ CoreMod.py
@@ -1,205 +1,193 @@
 import sys
 import os
-import itertools
 from itertools import combinations_with_replacement
-from itertools import permutations
 from collections import Counter
 
 """
 
     To Do:
-    
-    1 - add mutliple, symmetric sets as [[], []...]
-
+    1 - add flag for too many substitutions and too few total sites - Do in Comp.py
 
 """
 
 
-def COREMOD(core, core_mod, ligand_dict, mod_dict, param_dict):
+def COREMOD(core, core_mod, mod_dict, param_dict):
 
     structure_count = 0
 
     numsub = mod_dict['Number of Substitutions']
     mods   = mod_dict['Modifications']
 
+    sub_counts = [0 for i in range(max(numsub))]
+
+    if 'replig' not in param_dict:
+        param_dict['replig'] = 'true'
+    if 'ffoption' not in param_dict:
+        param_dict['ffoption'] = 'no'
+
+    if not os.path.exists(core + "/" + "/xyz"):
+        os.mkdir(core + "/" + "/xyz")
+    if not os.path.exists(core + "/" + "/mol_files"):
+        os.mkdir(core + "/" + "/mol_files")
+    if not os.path.exists(core + "/" + "/runs"):
+        os.mkdir(core + "/" + "/runs")
+
+    sym_H, nonsym_H, all_H = [], [], []
+
+    for k, v in core_mod.items():
+
+        if k == 'Symmetric Hs':
+            sym_H.append(v)
+        if k == 'Non-Symmetric Hs':
+            nonsym_H.append(v)
+        if k == 'All Hs':
+            all_H.append(v)
+    
+    h_s = []
+    h_s.append(all_H)
+    h_s.append(sym_H)
+    h_s.append(nonsym_H)
+
+    if os.path.exists(core + "/" + "/Structures"):
+        os.system("rm " + core + "/" + "/Structures")
+
+    for i in numsub:
+        i = int(i)
+        sub_count = 0
+
+        with open(core + "/" + "/Structures", "a+") as f:
+            f.write("Numer of Substitutions - " + str(i) + "\n\n")
+
+        record = []
+
+        for j in range(3):
+            temp = h_s[j]
+            if temp != []:
+                for jj in temp:
+                    if type(jj) == list:
+                        if any(isinstance(el, list) for el in jj):
+                            for k in jj:
+                                if i ==1:
+                                    record.append(k[:i])
+                                else:
+                                    for kk in range(min(i, len(k))):
+                                        record.append(k[:i])
+                        else:
+                            for k in jj:
+                                record.append([k])
+                    else:
+                        record.append([jj])
 
 
-    for envir_num, lig_envir in ligand_dict.items():
+        mod_perm = list(combinations_with_replacement(mods, i))
+        ind_perm = list(combinations_with_replacement(record, i))
 
-        if not os.path.exists(core + "/" + str(envir_num)):
-            os.mkdir(core + "/" + str(envir_num))
-        if not os.path.exists(core + "/" + str(envir_num) + "/xyz"):
-            os.mkdir(core + "/" + str(envir_num) + "/xyz")
-        if not os.path.exists(core + "/" + str(envir_num) + "/mol_files"):
-            os.mkdir(core + "/" + str(envir_num) + "/mol_files")
-        if not os.path.exists(core + "/" + str(envir_num) + "/runs"):
-            os.mkdir(core + "/" + str(envir_num) + "/runs")
-        
-        print ("Environment Number: " + str(envir_num))
-        print ()
+        temp = []
+        for j in ind_perm:
+            if j not in temp:
+                flag = True
+                for jj in j:
+                    if len(jj) < j.count(jj):
+                        flag = False
+                if flag == True:
+                    temp.append(j)
 
+        ind_perm = temp
 
-        all_H    = {}
-        sym_H    = {}
-        nonsym_H = {}
+        unique = []
 
-        for lig, lig_info in lig_envir.items():
+        for j in ind_perm:
+            for k in mod_perm:
+                count = list(Counter(k))
+                if len(count) < int(i):
+                    jj = sorted(j)
+                else:
+                    jj = j
+                k_jj = [k, jj]
+                if k_jj not in unique:
+                    unique.append(k_jj)
 
-            if 'All Hs' in lig_info:
-                all_H = []
-                lig_info['All Hs']
-                for k in lig_info['All Hs']:
-                    all_H.append(k)
-                ALLH = True
+        for j in unique:
+            sub = j[0]
+            ind = j[1]
+
+            with open(core + "/" + "/Structures", "a+") as f:
+
+                sub_txt = ''
+                ind_txt = ''
+
+                for jj in range(i):
+                    sub_txt += str(sub[jj]) + " "
+                    ind_txt += str(ind[jj]) + " "
+
+                f.write(sub_txt + "\tsubstitutions on sites " + ind_txt + "\n")
+
+            with open(core + "/" + "/mol_files/" + str(structure_count) + ".mol", "w") as f:
+
+                f.write("-core " + core + "\n")
+                f.write("-rundir " + core + "/runs/\n")
+
+                f.write("-lig ")
+                for j in sub:
+                    f.write(j + " ")
+                f.write("\n")
+
+                f.write("-ligocc ")
+                for j in range(i):
+                    f.write("1" + " ")
+                f.write("\n")
+
+                f.write("-ccatoms ")
+                temp = ''
+                for j in ind:
+                    temp += (str(j) + ",")
+                f.write(temp[:-1])
+                f.write("\n")
+
+                for k, v in param_dict.items():
+                    f.write("-" + str(k) + " " + str(v) + "\n")
+
+            if sys.argv[1] == 'compile':
+                os.system("molsimplify -i " + core + "/" + "/mol_files/" + str(structure_count) + ".mol")
+                os.system("cp " + core + "/" + "/runs/*/*/*.xyz" + \
+                          " " + core + "/" + "/xyz/" + str(structure_count) + ".xyz")
+                os.system("rm -r " + core + "/" + "/runs/*")
 
             else:
-                ALLH = False
-                sym_H[lig]    = []
-                nonsym_H[lig] = []
+                pass
 
-                try:
-                    lig_info['Symmetric Hs']
-                    for k in lig_info['Symmetric Hs']:
-                        sym_H[lig].append(k)
-
-                except:
-                    pass
-
-                try:
-                    lig_info['Non-Symmetric Hs']
-                    for k in lig_info['Non-Symmetric Hs']:
-                        nonsym_H[lig].append(k)
-
-                except:
-                    pass
-
-
-        for i in numsub:
-
-            if ALLH == False:
-                
-                front = []
-                all_H = []
-
-                for k, v in sym_H.items():
-
-                    if v != []:
-                        front = v[:int(i)]
-                        all_H.append(front)
-
-                for k, v in nonsym_H.items():
-
-                    if v != []:
-                        all_H.append(v)
-
-            all_H = list(itertools.chain.from_iterable(all_H))
-
-            print ("Number of Substitutions: " + i)
-            print ()
-            mod_perm = list(combinations_with_replacement(mods, int(i)))
-            ind_perm = list(permutations(all_H, int(i)))
+            structure_count += 1
+            sub_count += 1
         
-            
-            if list(itertools.chain.from_iterable(ind_perm)) == []:
+        sub_counts[i-1] = sub_count
 
-                print ("Congratulations. Somehow you got this far with too many substitutions and too few")
-                print ("substitution sites. Don't do that again.")
-                print ("Skipping environment #%s." %i)
-                print ()
-                break
+        with open(core + "/" + "/Structures", "a+") as f:
+            f.write("\n%s structures created using %s substitutions" %(sub_count, i))
+            f.write("\n\n")
 
-
-
-            unique = []
-
-            for j in ind_perm:
-                for k in mod_perm:
-                    count = list(Counter(k))
-                    if len(count) < int(i):
-                        jj = sorted(j)
-                    else:
-                        jj = j
-                    k_jj = [k, jj]
-                    if k_jj not in unique:
-                        unique.append(k_jj)
-
-            for j in unique:
-                sub = j[0]
-                ind = j[1]
-
-                subs = ''
-                for k in sub:
-                    subs += str(k) + "_"
-                subs = subs[:-1]
-
-                inds = ''
-                for k in ind:
-                    inds += str(k) + "_"
-                inds = inds[:-1]
-
-                with open(core + "/" + str(envir_num) + "/mol_files/" + "%s_%s.mol" %(subs, inds), "w") as f:
-
-                    f.write("-core "   + core + "\n")
-                    f.write("-lig ")
-                    for lig, lig_info in lig_envir.items():
-                        f.write(lig + " ")
-                    f.write("\n")
-                    f.write("-ligocc ")
-                    for lig, lig_info in lig_envir.items():
-                        f.write(str(lig_info['Number of Ligands']) + " ")
-                    f.write("\n")
-                    f.write("-rundir " + core + "/" + str(envir_num) + "/runs/\n")
-                    f.write("-geometry oct\n")
-
-                    temp = '-decoration ['
-                    for k in sub:
-                        temp += str(k) + ','
-                    temp = temp[:-1]
-                    temp += ']\n'
-                    f.write(temp)
-                    temp = '-decoration_index ['
-                    for k in ind:
-                        temp += str(k) + ','
-                    temp = temp[:-1]
-                    temp += ']\n'
-                    f.write(temp)
+    for j in numsub:
+        print ("%s \ttotal structures generated with %s substitutions" %(sub_counts[j-1], j))
+    print ("%s \ttotal structures generated" %(structure_count))
 
 
-                os.system("molsimplify -i " + core + "/" + str(envir_num) + "/mol_files/" + "%s_%s.mol" % (subs, inds))
-                os.system("cp " + core + "/" +  str(envir_num) + "/runs/*/*/*.xyz" + \
-                          " " + core + "/" + str(envir_num) + "/xyz/" + "%s_%s.xyz" % (subs, inds))
-                os.system("rm -r " + core + "/" + str(envir_num) + "/runs/*")
-
-                structure_count += 1
-
-    print ("%s total structures generated" %structure_count)
-
-    
     return
-
-
 
 
 if __name__ == "__main__":
 
+    core_dict   = {
+                   'nickelporphyrin1': {'All Hs': [9, 10, 17, 18, 24, 25, 30, 31]},
+                   'nickelporphyrin2': {'Symmetric Hs': [[9, 10], [17, 18], [24, 25]], 'Non-Symmetric Hs': [30, 31]}
+                   }
 
-    #"""
-    ligand_dict = {0:
-                  {'acac': {'Number of Ligands': '1', 'Symmetric Hs': [8, 9, 10], 'Non-Symmetric Hs': [14]},
-                   'no3': {'Number of Ligands': '2'}}, 
-                  1:
-                  {'acac': {'Number of Ligands': '2', 'Symmetric Hs': [8, 9, 10], 'Non-Symmetric Hs': [14]},
-                   'no3': {'Number of Ligands': '1'}},
-                  2:
-                  {'acac': {'Number of Ligands': '3', 'Symmetric Hs': [8, 9, 10], 'Non-Symmetric Hs': [14]}}}
+    mod_dict    = {'Modifications': ['f', 'cl', 'br', 'i'], 'Number of Substitutions': [1, 2, 3, 4, 5]}
 
-    #"""
+    param_dict = {'geometry': 'sqp'}
 
-    #ligand_dict = {0:{'acac':{'Number of Ligands': '3', 'All Hs': [8, 9, 10, 11]}}}
+    for core, core_mod in core_dict.items():
+        print (core)
 
-    core        = 'la'
+        if not os.path.exists(core):
+            os.mkdir(core)
 
-    mod_dict    = {'Modifications': ['f', 'cl', 'br'], 'Number of Substitutions': ['1']}
-
-
-    COREMOD(core, core_mod, ligand_dict, mod_dict, param_dict)
+        COREMOD(core, core_mod, mod_dict, param_dict)
